865704797e26e4ede6be92236029de534b5251d9
Enes Koç <crestand@windowslive.com>
2023-06-18 16:52:06 +0300

    Level design

--- Assets/Prefabs/ObstaclePrefabs/RotatingPlatformController.cs
+++ Assets/Prefabs/ObstaclePrefabs/RotatingPlatformController.cs
@@ -8,6 +8,8 @@ public class RotatingPlatformController : MonoBehaviour
 
     [SerializeField] float rotateTime;
     [SerializeField] GameObject platformModel;
+    [SerializeField] RotationDirection rotationDirection;
+
     public Vector3 rotatingPlatformDestination;
     void Start()
     {
@@ -16,12 +18,18 @@ public class RotatingPlatformController : MonoBehaviour
 
     void rotatingPlatform()
     {
-        platformModel.transform.DORotate(new Vector3(0, 0, 360), rotateTime, RotateMode.FastBeyond360).SetLoops(-1, LoopType.Restart).SetEase(Ease.Linear);
+
+        platformModel.transform.DORotate(new Vector3(0, 0, (float)rotationDirection), rotateTime, RotateMode.FastBeyond360).SetLoops(-1, LoopType.Restart).SetEase(Ease.Linear);
     }
 
     private void OnDrawGizmos() {
         Gizmos.color=Color.red;
         Gizmos.DrawWireSphere(rotatingPlatformDestination,0.6f);
+    }
+
+    enum RotationDirection{
+        Left = 360,
+        Right=-360
     }
 
 }
